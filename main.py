01895d97aaa90a8d0674fed0b460c98caeaa2d20
erik <tomerik.s@gso.schule.koeln>
2026-02-12 10:52:21 +0100

    ich habs fertig gemacht du hund

--- main.py
+++ main.py
@@ -1,1 +1,99 @@
+def dingsfunktion0():
+    print("Was sind die Dinger?")
+    print("länge: ")
+    dings1 = int(input())
+    print("breite: ")
+    dings2 = int(input())
+    return dings1 * dings2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dingsArray0 = []
+
 print("Hello World!")
+
+
+
+
+while True: 
+    print("Gibt es noch unvermessene Räume? (y/n)")
+    dings0 = input()
+
+    if dings0 == 'n':
+        break
+
+    dings3 = dingsfunktion0()
+    dingsArray0.append(dings3)
+
+
+print(dingsArray0)
+
+
