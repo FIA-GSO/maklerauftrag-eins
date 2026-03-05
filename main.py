68101bb5fc2c056bb8eafe6e830005d7b8cbe046
erik <tomerik.s@gso.schule.koeln>
2026-03-05 09:54:01 +0100

    made program

--- main.py
+++ main.py
@@ -1,87 +1,99 @@
-def dingsfunktion0() -> int:
-    print("Was sind die Dinger?")
-    print("länge: ")
-    dings1 = int(input())
-    print("breite: ")
-    dings2 = int(input())
-    return dings1 * dings2
+"""implementation of the Maklerauftrag"""
 
-dingsArray0 = []
+class MaklerAuftrag():
+    """maklerauftrag class"""
+    def __init__(self):
+        # struktur:
+        # {
+        # "raum1": [(2, 3), (4, 5)],
+        # "raum2": [(2, 3)],
+        # "raum3": [(2, 3), (4, 5), (5, 6)],
+        # }
+        self.wohnung = {}
 
-print("Hello World!")
+    def calculate_room_area(self, name) -> float:
+        """calculates area of a single room"""
+        pairs = self.wohnung.get(name, [])
+        return sum(a * b for a, b in pairs)
 
-while True:
-    print("Gibt es noch unvermessene Räume? (y/n)")
-    dings0 = input()
+    def calculate_total_area(self) -> float:
+        """calculates the total area"""
+        return sum(self.calculate_room_area(name) for name in self.wohnung)
 
-    if dings0 == 'n':
-        break
+    def create_new_room(self, name):
+        """creates new room"""
 
-    dings3 = dingsfunktion0()
-    dingsArray0.append(dings3)
+        if name in self.wohnung:
+            print("Raum existiert schon")
+            return
 
-print(dingsArray0)
+        self.wohnung[name] = []
 
+        while True:
+            values = input("> ").split()
+            if not values:
+                continue
+            if values[0] == "done":
+                break
+            if len(values) > 2:
+                print("Bitte zwei Werte Eingeben")
+            try:
+                self.wohnung[name].append((float(values[0]), float(values[1])))
+            except (IndexError, ValueError):
+                print("Wrong input")
 
-while True:
-    new, delete, log, done
+        print(f"Neuen Raum erstellt: {name} {self.wohnung[name]}\n")
 
-    new <raumname>: 
-        wie viele Rechtecke im nächsten Raum?
+    def delete_room(self, name):
+        """deletes room"""
+        if name not in self.wohnung:
+            print("Raum existiert nicht\n")
+            return
 
-        Seitenlängen eingeben
+        del self.wohnung[name]
+        print(f"Raum gelöscht: {name}\n")
 
-        kein weiterer Raum?
-        -> Beenden + Ergebnisse ausgeben
-    
-    delete <raumname>:
-        löscht den Raum
-    
-    log <raumname>: 
-        gibt die werte der Rechtecke des Raumes aus
-    
-    log: 
-        gibt die Räume und gesammtfläche aus
-    
-    done: 
-        Gibt alle Werte der Wohnung aus. 
+    def list_rooms(self):
+        """lists all rooms"""
+        if not self.wohnung:
+            print("Keine Räume hizugefügt\n")
+            return
 
+        for name, pairs in self.wohnung.items():
+            print(name + ": ")
 
-Wohnuuuuung = {
-    "raum1": [(2, 3), (4, 5)],
-    "raum2": [(2, 3)],
-    "raum3": [(2, 3), (4, 5), (5, 6)],
-    "raum4": [(2, 3), (4, 5)],
-}
+            if not pairs:
+                print("  Keine Werte hinzugefügt")
+            else:
+                for i, (a, b) in enumerate(pairs, 1):
+                    print(f" {i}: {a} * {b} = {a*b}")
+                print(f"Raum Fläche: {self.calculate_room_area(name)}\n")
 
-wohnung = {}
+        print(f"Gesamtfläche: {self.calculate_total_area()}\n")
 
-def isEmpty(word) -> boolean:
-    return not word
+    def run(self):
+        """main method"""
 
-def getUserinput() -> dict:
-    userinput = input()
-    if isEmpty(userinput):
-        return getUserinput()
-    userinput = userinput.split(' ', 1)
-    return {
-        "befehl": userinput[0], 
-        "name": userinput[1] if isEmpty(userinput[1]) else ''
-    }
+        print("""Willkommen
+              """)
 
-def createNewRoom():
-    
+        while True:
+            userinput = input().split()
+            if not userinput:
+                continue
 
+            if userinput[0] == "new":
+                if len(userinput) == 2:
+                    self.create_new_room(userinput[1])
+            elif userinput[0] == "delete":
+                if len(userinput) == 2:
+                    self.delete_room(userinput[1])
+            elif userinput[0] == "list":
+                if len(userinput) == 1:
+                    self.list_rooms()
+            elif userinput[0] == "done":
+                if len(userinput) == 1:
+                    return
 
-while True: 
-    userinput = getUserinput()
-    
-    match userinput["befehl"]: 
-        case "new":
-            print(userinput)
-        case "delete":
-            print(userinput)
-        case "log":
-            print(userinput)
-        case "done":
-            print(userinput)
+dings = MaklerAuftrag()
+dings.run()
